7c07c6bf8ad7797f1f6ff33b4ff5aeff2d0d4b3b
elaynefmelo <elayne.mello6@gmail.com>
2024-07-18 12:41:50 -0300

    Tela Video nivel basico

--- src/app/nivelIntroducao/video.tsx
+++ src/app/nivelIntroducao/video.tsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet, GestureResponderEvent, Alert } from 'react-native';
-import { styles } from "./styles";
+import { View, Alert } from 'react-native';
 import Header from '@/src/components/Header';
 import BackButton from '@/src/components/BackButton';
 import BlocoQuestoes from '@/src/components/BlocoQuestoes';
-import VerifyButton from '@/src/components/VerifyButton';
+import VideoComponent from '@/src/components/VideoComponent';
+import { styles } from './styles';
 
 const Video = () => {
 
   const handleVerifyPress = () => {
     Alert.alert('Verificado!', 'Você acertou a resposta.');
   };
-  
+
   return (
     <View style={styles.container}>
       <Header
-          title="Vídeo"
-          iconSource={require('@/src/assets/Video.png')}
-        />
+        title="Vídeo"
+        iconSource={require('@/src/assets/Video.png')}
+      />
       <BackButton />
-      <BlocoQuestoes title={'Vamos assistir um vídeo e aprender'} />
-      <VerifyButton onPress={function (event: GestureResponderEvent): void {
-        throw new Error('Function not implemented.');
-      } }/>
+      <View style={styles.contentContainer}>
+        <BlocoQuestoes title={'Vamos assistir um vídeo e aprender'} />
+        <VideoComponent moduleId={1} />
+      </View>
     </View>
   );
 };
